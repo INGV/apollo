7a2f79ea9b99d75b3a935e1503e3959c3de82b93
Valentino Lauciani <valentino.lauciani@ingv.it>
2022-04-05 17:48:36 +0200

    Update

--- apollo/configure_laradock.sh
+++ apollo/configure_laradock.sh
@@ -87,6 +87,14 @@ checkReturnCode ${?}
 echo "Done"
 echo ""
 
+# FIX BUG on COMPOSER_AUTH: https://github.com/laradock/laradock/pull/3190
+echo "!!! - FIX BUG on COMPOSER_AUTH: https://github.com/laradock/laradock/pull/3190 :"
+git checkout master
+git fetch origin refs/pull/3190/head:pr_3190
+git checkout pr_3190
+echo "Done"
+echo ""
+
 # Update Laradock 'docker-compose.yml'. Issue: https://github.com/laradock/laradock/issues/2947
 echo "Update Laradock 'docker-compose.yml':"
 FILE_DOCKERCOMPOSE="docker-compose.yml"
