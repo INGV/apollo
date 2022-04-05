6f5cfdd01deacbe97e1ea6901ff046398109a761
Valentino Lauciani <valentino.lauciani@ingv.it>
2022-04-05 19:10:13 +0200

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Log::info(' ----- NEW REQUEST ----- ');
+        \Log::info(' $request->getMethod()                  = ' . $this->app->request->getMethod());
+        \Log::info(' $request->fullUrl()                    = ' . $this->app->request->fullUrl());
+        \Log::info(' $request->all()                        = ',  $this->app->request->all());
+        \Log::info(' $request->getSchemeAndHttpHost()       = ' . $this->app->request->getSchemeAndHttpHost());
+        \Log::info(' $request->ip()                         = ' . $this->app->request->ip());
+
+        /**
+         * The 'App\Http\Middleware/TrustProxies' is executed AFTER this 'AppServiceProvider' then the '$this->app->request->ip()'
+         *  doesn't return the 'client' IP - from: header('x-forwarded-for') - but return the 'proxy' IP. To solve this problem I'll
+         *  get the origina 'client' IP manually
+         */
+        if ($this->app->request->headers->has('x-forwarded-for')) {
+            $clientIp = $this->app->request->headers->get('x-forwarded-for');
+        } else {
+            $clientIp = $this->app->request->ip();
+        }
+        \Log::info(' $clientIp from "x-forwarded-for"   = ' . $clientIp . ' -> ' . gethostbyaddr($clientIp));
+        \Log::info(' $request->header()                 = ',  $this->app->request->header());
     }
 }
