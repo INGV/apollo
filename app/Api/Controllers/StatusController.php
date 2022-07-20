9a28b956bd2f29f3a7137edd95b9ed9635174b84
Valentino Lauciani <valentino.lauciani@ingv.it>
2022-07-20 12:00:00 +0200

    Fix indentation by 'laravel/pint' package

--- app/Api/Controllers/StatusController.php
+++ app/Api/Controllers/StatusController.php
@@ -6,22 +6,20 @@ use App\Http\Controllers\Controller;
 
 class StatusController extends Controller
 {
-
-    /**
-     * 
-     */
     public function index()
     {
-        $status         = 200;
-        $statusMessage  = \Symfony\Component\HttpFoundation\Response::$statusTexts[$status] ? \Symfony\Component\HttpFoundation\Response::$statusTexts[$status] : '--';
+        $status = 200;
+        $statusMessage = \Symfony\Component\HttpFoundation\Response::$statusTexts[$status] ? \Symfony\Component\HttpFoundation\Response::$statusTexts[$status] : '--';
         if ($status == 200) {
-            $message = "The service is working properly";
-        } else
+            $message = 'The service is working properly';
+        } else {
             $message = $statusMessage;
+        }
+
         return response([
-            'status'    => $status,
-            'title'     => $statusMessage,
-            'detail'    => $message,
+            'status' => $status,
+            'title' => $statusMessage,
+            'detail' => $message,
         ])->header('Content-Type', 'application/problem+json');
     }
 }
--- app/Api/v2/Controllers/PyMLController.php
+++ app/Api/v2/Controllers/PyMLController.php
@@ -3,14 +3,13 @@
 namespace App\Api\v2\Controllers;
 
 use App\Api\v2\Models\PyMLModel;
-use Illuminate\Support\Facades\Log;
 use App\Api\v2\Requests\PyMLRequest;
-use App\Http\Controllers\Controller;
-use Symfony\Component\Process\Process;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Validator;
 use App\Api\v2\Traits\FindAndRetrieveStationXMLTrait;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class PyMLController extends Controller
 {
@@ -19,48 +18,47 @@ class PyMLController extends Controller
     protected $default_pyml_conf = [
         'iofilenames' => [
             'magnitudes' => '/opt/data/pyml_magnitudes.csv',
-            'log' => '/opt/data/pyml_general.log'
+            'log' => '/opt/data/pyml_general.log',
         ],
         'preconditions' => [
             'theoretical_p' => false,
             'theoretical_s' => false,
             'delta_corner' => 5,
-            'max_lowcorner' => 15
+            'max_lowcorner' => 15,
         ],
         'station_magnitude' => [
             'delta_peaks' => 1,
             'use_stcorr_hb' => true,
             'use_stcorr_db' => true,
             'when_no_stcorr_hb' => true,
-            'when_no_stcorr_db' => true
+            'when_no_stcorr_db' => true,
         ],
         'event_magnitude' => [
             'mindist' => 10,
             'maxdist' => 600,
             'hm_cutoff' => [
                 12,
-                13
+                13,
             ],
             'outliers_max_it' => 10,
             'outliers_red_stop' => 0.1,
             'outliers_nstd' => 1,
-            'outliers_cutoff' => 0.1
-        ]
+            'outliers_cutoff' => 0.1,
+        ],
     ];
 
     /**
      * Checks if multiple keys exist in an array
      * https://www.geeksforgeeks.org/how-to-search-by-multiple-key-value-in-php-array/
      *
-     * @param array $array
-     * @param array $keys
-     *
+     * @param  array  $array
+     * @param  array  $keys
      * @return array
      */
     public static function array_keys_exist($array, $search_list)
     {
         // Create the result array
-        $result = array();
+        $result = [];
 
         // Iterate over each array element
         foreach ($array as $key => $value) {
@@ -69,7 +67,7 @@ class PyMLController extends Controller
                 // If the array element does not meet
                 // the search condition then continue
                 // to the next element
-                if (!isset($value[$k]) || $value[$k] != $v) {
+                if (! isset($value[$k]) || $value[$k] != $v) {
                     // Skip two loops
                     continue 2;
                 }
@@ -78,7 +76,7 @@ class PyMLController extends Controller
             //result array
             $result[] = $value;
         }
-        // Return result 
+        // Return result
         return $result;
     }
 
@@ -88,7 +86,7 @@ class PyMLController extends Controller
      */
     public function location(PyMLRequest $request)
     {
-        Log::debug("START - " . __CLASS__ . ' -> ' . __FUNCTION__);
+        Log::debug('START - '.__CLASS__.' -> '.__FUNCTION__);
         $locationTimeStart = microtime(true);
 
         /* Get validated input */
@@ -103,10 +101,10 @@ class PyMLController extends Controller
         $tmpAmplitudeChaComponents = [];
         $nAmplitudes = count($input_parameters['data']['amplitudes']);
         foreach ($input_parameters['data']['amplitudes'] as &$amplitude) {
-            $pyMLCoordArray = PyMLModel::getCoord($amplitude, config('apollo.cacheTimeout'), $n . '/' . $nAmplitudes . ' - ');
+            $pyMLCoordArray = PyMLModel::getCoord($amplitude, config('apollo.cacheTimeout'), $n.'/'.$nAmplitudes.' - ');
 
             if (empty($pyMLCoordArray)) {
-                Log::debug(" No, coordinates");
+                Log::debug(' No, coordinates');
             } else {
                 /* Add coord to amplitude */
                 $amplitude['lat'] = $pyMLCoordArray['lat'];
@@ -118,7 +116,7 @@ class PyMLController extends Controller
                 $sta = $amplitude['sta'];
                 $cha = $amplitude['cha'];
                 $loc = $amplitude['loc'] ?? '--';
-                $tmpAmplitudeChaComponents[$net . '.' . $sta . '.' . $loc . '.' . substr($cha, 0, 2)][] = substr($cha, 2, 1); // $tmpAmplitudeChaComponents['IV.ACER.--.HH'] => ['N', 'E']
+                $tmpAmplitudeChaComponents[$net.'.'.$sta.'.'.$loc.'.'.substr($cha, 0, 2)][] = substr($cha, 2, 1); // $tmpAmplitudeChaComponents['IV.ACER.--.HH'] => ['N', 'E']
             }
             $n++;
         }
@@ -140,15 +138,15 @@ class PyMLController extends Controller
         /****** END - pyml_conf ******/
 
         /* Set variables */
-        $now            = \DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
-        $nowFormatted   = $now->format("Ymd_His");
-        $random_name    = $nowFormatted . "__" . gethostbyaddr(\request()->ip()) . "__" . \Illuminate\Support\Str::random(5);
-        $dir_working    = "/pyml/" . $random_name;
-        $dir_data       = config('filesystems.disks.data.root');
+        $now = \DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
+        $nowFormatted = $now->format('Ymd_His');
+        $random_name = $nowFormatted.'__'.gethostbyaddr(\request()->ip()).'__'.\Illuminate\Support\Str::random(5);
+        $dir_working = '/pyml/'.$random_name;
+        $dir_data = config('filesystems.disks.data.root');
 
         /* Write input.json */
-        $file_input_json            = "input.json";
-        $file_input_fullpath_arc    = $dir_working . "/" . $file_input_json;
+        $file_input_json = 'input.json';
+        $file_input_fullpath_arc = $dir_working.'/'.$file_input_json;
         Storage::disk('data')->put($file_input_fullpath_arc, json_encode($input_parameters));
 
         /* !!!!!!!! START - Get 'id -u' ToDo better */
@@ -156,23 +154,23 @@ class PyMLController extends Controller
             array_merge(
                 [
                     'id',
-                    '-u'
+                    '-u',
                 ]
             );
 
         /* Run process */
-        Log::debug(" Running command: ", $command);
+        Log::debug(' Running command: ', $command);
         $command_timeout = 120;
         $command_process = new Process($command);
         $command_process->setTimeout($command_timeout);
         $command_process->run();
-        $uid = preg_replace("/\r|\n/", "", $command_process->getOutput());
-        Log::debug(" getOutput:" . $uid);
-        Log::debug(" getErrorOutput:" . $command_process->getErrorOutput());
-        if (!$command_process->isSuccessful()) {
+        $uid = preg_replace("/\r|\n/", '', $command_process->getOutput());
+        Log::debug(' getOutput:'.$uid);
+        Log::debug(' getErrorOutput:'.$command_process->getErrorOutput());
+        if (! $command_process->isSuccessful()) {
             throw new ProcessFailedException($command_process);
         }
-        Log::debug(" Done.");
+        Log::debug(' Done.');
         /* !!!!!!!! END - Get 'id -u' ToDo better */
 
         /* !!!!!!!! START - Get 'id -g' ToDo better */
@@ -180,23 +178,23 @@ class PyMLController extends Controller
             array_merge(
                 [
                     'id',
-                    '-g'
+                    '-g',
                 ]
             );
 
         /* Run process */
-        Log::debug(" Running command: ", $command);
+        Log::debug(' Running command: ', $command);
         $command_timeout = 120;
         $command_process = new Process($command);
         $command_process->setTimeout($command_timeout);
         $command_process->run();
-        $gid = preg_replace("/\r|\n/", "", $command_process->getOutput());
-        Log::debug(" getOutput:" . $gid);
-        Log::debug(" getErrorOutput:" . $command_process->getErrorOutput());
-        if (!$command_process->isSuccessful()) {
+        $gid = preg_replace("/\r|\n/", '', $command_process->getOutput());
+        Log::debug(' getOutput:'.$gid);
+        Log::debug(' getErrorOutput:'.$command_process->getErrorOutput());
+        if (! $command_process->isSuccessful()) {
             throw new ProcessFailedException($command_process);
         }
-        Log::debug(" Done.");
+        Log::debug(' Done.');
         /* !!!!!!!! END - Get 'id -g' ToDo better */
 
         /* !!!!!!!! START - Get 'docker run' ToDo better */
@@ -207,32 +205,32 @@ class PyMLController extends Controller
                     'run',
                     '--rm',
                     '--user',
-                    $uid . ':' . $gid,
-                    '-v', $dir_data . $dir_working . ":/opt/data",
+                    $uid.':'.$gid,
+                    '-v', $dir_data.$dir_working.':/opt/data',
                     config('apollo.docker_pyml'),
-                    '--json', '/opt/data/input.json'
+                    '--json', '/opt/data/input.json',
                 ]
             );
 
         /* Run process */
-        Log::debug(" Running docker: ", $command);
+        Log::debug(' Running docker: ', $command);
         $command_timeout = 120;
         $command_process = new Process($command);
         $command_process->setTimeout($command_timeout);
         $command_process->run();
-        Log::debug(" getOutput:" . $command_process->getOutput());
-        Log::debug(" getErrorOutput:" . $command_process->getErrorOutput());
-        if (!$command_process->isSuccessful()) {
+        Log::debug(' getOutput:'.$command_process->getOutput());
+        Log::debug(' getErrorOutput:'.$command_process->getErrorOutput());
+        if (! $command_process->isSuccessful()) {
             throw new ProcessFailedException($command_process);
         }
-        Log::debug(" Done.");
+        Log::debug(' Done.');
         /* !!!!!!!! END - Get 'docker run' ToDo better */
 
         /* */
-        $file_output_log                = "output.log";
-        $file_output_err                = "output.err";
-        $file_output_fullpath_log       = $dir_working . "/" . $file_output_log;
-        $file_output_fullpath_err       = $dir_working . "/" . $file_output_err;
+        $file_output_log = 'output.log';
+        $file_output_err = 'output.err';
+        $file_output_fullpath_log = $dir_working.'/'.$file_output_log;
+        $file_output_fullpath_err = $dir_working.'/'.$file_output_err;
 
         /* Write warnings and errors into log file */
         Log::debug(" Write warnings and errors into \"$file_output_fullpath_err\"");
@@ -250,45 +248,46 @@ class PyMLController extends Controller
         */
 
         if ($output_format == 'text') {
-            $contents = Storage::disk('data')->get($dir_working . "/pyml_magnitudes.csv");
+            $contents = Storage::disk('data')->get($dir_working.'/pyml_magnitudes.csv');
             /* set headers */
             $headers['Content-type'] = 'text/plain';
+
             return response()->make($contents, 200, $headers);
         } else {
             /* Get pyml csv file */
             $csvToArray = [];
-            if (($open = fopen($dir_data . $dir_working . '/pyml_magnitudes.csv', "r")) !== FALSE) {
+            if (($open = fopen($dir_data.$dir_working.'/pyml_magnitudes.csv', 'r')) !== false) {
                 while (($data = fgetcsv(
                     $open,
                     1000,
-                    ";"
-                )) !== FALSE) {
+                    ';'
+                )) !== false) {
                     $csvToArray[] = $data;
                 }
                 fclose($open);
             }
 
             /* Build output */
-            $output['data']['random_string']    = $random_name;
+            $output['data']['random_string'] = $random_name;
             //$output['data']['eventid']          = $csvToArray[1][0];
 
             /* START - Magnitudes */
             $output['data']['magnitudes'] = [
                 'hb' => [
-                    'ml'        => $csvToArray[1][1],
-                    'std'       => $csvToArray[1][2],
-                    'totsta'    => $csvToArray[1][3],
-                    'usedsta'   => (string)intval($csvToArray[1][4]),
+                    'ml' => $csvToArray[1][1],
+                    'std' => $csvToArray[1][2],
+                    'totsta' => $csvToArray[1][3],
+                    'usedsta' => (string) intval($csvToArray[1][4]),
                 ],
                 'db' => [
-                    'ml'        => $csvToArray[1][5],
-                    'std'       => $csvToArray[1][6],
-                    'totsta'    => $csvToArray[1][7],
-                    'usedsta'   => (string)intval($csvToArray[1][8]),
+                    'ml' => $csvToArray[1][5],
+                    'std' => $csvToArray[1][6],
+                    'totsta' => $csvToArray[1][7],
+                    'usedsta' => (string) intval($csvToArray[1][8]),
                 ],
-                'ampmethod'     => $csvToArray[1][9],
-                'magmethod'     => $csvToArray[1][10],
-                'loopexitcondition' => $csvToArray[1][11]
+                'ampmethod' => $csvToArray[1][9],
+                'magmethod' => $csvToArray[1][10],
+                'loopexitcondition' => $csvToArray[1][11],
             ];
             /* END - Magnitudes */
 
@@ -296,7 +295,7 @@ class PyMLController extends Controller
             unset($csvToArray[0]);  // Remove header
             unset($csvToArray[1]);  // Remove origin magnitude
             foreach ($csvToArray as $csvToArrayLine) {
-                list($mlcha, $scnl, $ml_hb, $ml_hb_weight, $e, $ml_db, $ml_db_weight) = explode(" ", $csvToArrayLine[0]);
+                [$mlcha, $scnl, $ml_hb, $ml_hb_weight, $e, $ml_db, $ml_db_weight] = explode(' ', $csvToArrayLine[0]);
 
                 /* Get SCNL line */
                 $search_items = [];
@@ -308,16 +307,16 @@ class PyMLController extends Controller
                 } else {
                     $loc = $scnl_exploded[2];
                     $search_items['loc'] = $loc;
-                };
+                }
                 $cha = $scnl_exploded[3];
                 $search_items['net'] = $net;
                 $search_items['sta'] = $sta;
                 $search_items['cha'] = $cha;
 
-                /** Search items, into: 
-                 *   '$input_parameters['data']['amplitudes']' 
-                 *  with: 
-                 *   'net=$net', 'sta=$sta', 'cha=$cha', (optional 'loc=$loc'). 
+                /** Search items, into:
+                 *   '$input_parameters['data']['amplitudes']'
+                 *  with:
+                 *   'net=$net', 'sta=$sta', 'cha=$cha', (optional 'loc=$loc').
                  */
                 $inputAmplitude = self::array_keys_exist($input_parameters['data']['amplitudes'], $search_items);
 
@@ -336,12 +335,10 @@ class PyMLController extends Controller
             /* END - Stationmagnitude */
 
             $locationExecutionTime = number_format((microtime(true) - $locationTimeStart) * 1000, 2);
-            Log::info("END - " . __CLASS__ . ' -> ' . __FUNCTION__ . ' | locationExecutionTime=' . $locationExecutionTime . ' Milliseconds');
+            Log::info('END - '.__CLASS__.' -> '.__FUNCTION__.' | locationExecutionTime='.$locationExecutionTime.' Milliseconds');
+
             return response()->json($output, 200, [], JSON_PRETTY_PRINT);
         }
-
-
-
 
         /*
         $amplitude = [];
@@ -392,7 +389,7 @@ class PyMLController extends Controller
         }
         $locationExecutionTime = number_format((microtime(true) - $locationTimeStart) * 1000, 2);
         Log::info("END - " . __CLASS__ . ' -> ' . __FUNCTION__ . ' | locationExecutionTime=' . $locationExecutionTime . ' Milliseconds');
-        return response()->json($input_parameters, 200, [], JSON_PRETTY_PRINT);        
+        return response()->json($input_parameters, 200, [], JSON_PRETTY_PRINT);
         */
     }
 }
