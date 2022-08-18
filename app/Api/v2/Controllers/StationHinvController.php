51fb9299efb2de8a09713300e0e012610802c1ec
Valentino Lauciani <valentino.lauciani@ingv.it>
2022-08-18 15:57:15 +0200

    Formatting update

--- app/Api/v2/Controllers/StationHinvController.php
+++ app/Api/v2/Controllers/StationHinvController.php
@@ -2,18 +2,16 @@
 
 namespace App\Api\v2\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use App\Http\Controllers\Controller;
 use App\Api\v2\Models\StationHinvModel;
-use Illuminate\Support\Facades\Validator;
 use App\Api\v2\Requests\StationHinvRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 
 class StationHinvController extends Controller
 {
     public function query(StationHinvRequest $request)
     {
-        Log::debug("START - " . __CLASS__ . ' -> ' . __FUNCTION__);
+        Log::debug('START - '.__CLASS__.' -> '.__FUNCTION__);
 
         /* From GET, process only '$parameters_permitted' */
         $requestOnly = $request->validated();
@@ -24,7 +22,8 @@ class StationHinvController extends Controller
         /* set headers */
         $headers['Content-type'] = 'text/plain';
 
-        Log::debug("END - " . __CLASS__ . ' -> ' . __FUNCTION__);
+        Log::debug('END - '.__CLASS__.' -> '.__FUNCTION__);
+
         return response()->make($data, 200, $headers);
     }
 }
