f75d0c8d407fc1b02e501d7179c47d52501096fc
Valentino Lauciani <valentino.lauciani@ingv.it>
2023-02-27 09:38:11 +0100

    Add script

---
+++ apollo/scripts/for_ci/checkDindAndDockerImages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+echo "START - $( basename $0 )"
+COUNT=1
+COUNT_LIMIT=20
+while [[ $( docker -v 2>&1 >/dev/null ) ]] && [[ "${COUNT}" -le "${COUNT_LIMIT}" ]]; do 
+    echo " ${COUNT}/${COUNT_LIMIT} - waiting docker-in-docker starts."
+    COUNT=$(( ${COUNT} + 1 ))
+    sleep 1
+done
+if [[ ! $( docker -v 2>&1 >/dev/null ) ]]; then
+    echo "A"
+    exit 1
+elif
+    echo "B"
+    exit 1
+fi
+echo "END - $( basename $0 )"
