87db8043b4161d30ec1826058fc0448f1d048c33
Valentino Lauciani <valentino.lauciani@ingv.it>
2023-02-27 11:14:03 +0100

    Update

--- apollo/scripts/for_ci/checkDindAndDockerImages.sh
+++ apollo/scripts/for_ci/checkDindAndDockerImages.sh
@@ -2,6 +2,7 @@
 
 
 echo "START - $( basename $0 )"
+#
 COUNT=1
 COUNT_LIMIT=20
 while [[ $( docker -v 2>&1 >/dev/null ) ]] && [[ "${COUNT}" -le "${COUNT_LIMIT}" ]]; do 
@@ -9,11 +10,36 @@ while [[ $( docker -v 2>&1 >/dev/null ) ]] && [[ "${COUNT}" -le "${COUNT_LIMIT}"
     COUNT=$(( ${COUNT} + 1 ))
     sleep 1
 done
-if [[ ! $( docker -v 2>&1 >/dev/null ) ]]; then
-    echo "A"
-    exit 1
-else
-    echo "B"
-    exit 1
+if (( ${COUNT} >= ${COUNT_LIMIT} )); then
+    exit 1;
+fi
+echo ""
+
+
+#
+COUNT=1;
+COUNT_LIMIT=50;
+while [[ $(docker images | grep hyp2000 2>&1 >/dev/null) ]] && [[ ${COUNT} -le ${COUNT_LIMIT} ]]; do
+    echo " ${COUNT}/${COUNT_LIMIT} - waiting hyp2000 starts." 
+    COUNT=$(( ${COUNT} + 1))
+    sleep 1
+done
+if (( ${COUNT} >= ${COUNT_LIMIT} )); then
+    docker images
+    exit 1;
+fi
+echo ""
+
+#
+COUNT=1;
+COUNT_LIMIT=50;
+while [[ $(docker images | grep pyml 2>&1 >/dev/null) ]] && [[ ${COUNT} -le ${COUNT_LIMIT} ]]; do
+    echo " ${COUNT}/${COUNT_LIMIT} - waiting pyml starts." 
+    COUNT=$(( ${COUNT} + 1))
+    sleep 1
+done
+if (( ${COUNT} >= ${COUNT_LIMIT} )); then
+    docker images
+    exit 1;
 fi
 echo "END - $( basename $0 )"
