c1cd62152742069c4fbcdd0bfe5b72ab33d9a9f5
Valentino Lauciani <valentino.lauciani@ingv.it>
2023-10-23 11:30:40 +0200

    Remove unused line

--- docker/apollo/extra/apollo01FixPermissions.sh
+++ docker/apollo/extra/apollo01FixPermissions.sh
@@ -3,7 +3,6 @@ BASENAME=$( basename $0 )
 
 echo "START - ${BASENAME} -> apollo01FixPermissions.sh"
 echo " Running: chown -R application:application /app/storage/"
-#chown -R application:application /app
 chown -R application:application /app/storage/
 echo " Running: chown -R application:application /app/bootstrap/cache/"
 chown -R application:application /app/bootstrap/cache/
