dbf9970c4ae235aec9d570efb00539f220442a2f
Valentino Lauciani <valentino.lauciani@ingv.it>
2023-10-25 11:11:27 +0200

    minor fix

--- docker/apollo/extra/apollo01FixPermissions.sh
+++ docker/apollo/extra/apollo01FixPermissions.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 BASENAME=$( basename $0 )
+DIRS_LIST="/app/storage/ /app/storage/data/ /app/storage/data/pyml/ /app/storage/data/hyp2000/"
 
 echo "START - ${BASENAME} -> apollo01FixPermissions.sh"
 
 #echo " Running: chown -R application:application /app/storage/"
 #chown -R application:application /app/storage/
 
-echo " Running: chown application:application /app/storage/"
-chown application:application /app/storage/
-
-echo " Running: chown application:application /app/storage/data/"
-chown application:application /app/storage/data/
-
-echo " Running: chown application:application /app/storage/data/pyml/"
-chown application:application /app/storage/data/pyml
-
-echo " Running: chown application:application /app/storage/data/hyp2000/"
-chown application:application /app/storage/data/hyp2000
-
 echo " Running: chown -R application:application /app/bootstrap/cache/"
 chown -R application:application /app/bootstrap/cache/
+
+for DIR in ${DIRS_LIST}; do
+    if [ ! -d ${DIR} ]; then
+        echo " Create dir: ${DIR}"
+        mkdir ${DIR}
+    fi
+    echo " Running: chown application:application ${DIR}"
+    chown application:application ${DIR}
+done
 
 echo "END - ${BASENAME} -> apollo01FixPermissions.sh"
 echo ""
