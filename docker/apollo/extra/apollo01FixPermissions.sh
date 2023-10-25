4300475b36a59222fa31d36ad00a462083641502
Valentino Lauciani <valentino.lauciani@ingv.it>
2023-10-25 10:42:32 +0200

    Update permissions

--- docker/apollo/extra/apollo01FixPermissions.sh
+++ docker/apollo/extra/apollo01FixPermissions.sh
@@ -2,9 +2,24 @@
 BASENAME=$( basename $0 )
 
 echo "START - ${BASENAME} -> apollo01FixPermissions.sh"
-echo " Running: chown -R application:application /app/storage/"
-chown -R application:application /app/storage/
+
+#echo " Running: chown -R application:application /app/storage/"
+#chown -R application:application /app/storage/
+
+echo " Running: chown application:application /app/storage/"
+chown application:application /app/storage/
+
+echo " Running: chown application:application /app/storage/data/"
+chown application:application /app/storage/data/
+
+echo " Running: chown application:application /app/storage/data/pyml/"
+chown application:application /app/storage/data/pyml
+
+echo " Running: chown application:application /app/storage/data/hyp2000/"
+chown application:application /app/storage/data/hyp2000
+
 echo " Running: chown -R application:application /app/bootstrap/cache/"
 chown -R application:application /app/bootstrap/cache/
+
 echo "END - ${BASENAME} -> apollo01FixPermissions.sh"
 echo ""
